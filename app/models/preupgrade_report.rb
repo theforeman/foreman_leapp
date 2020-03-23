66fa224998b197072c5e3d52643ccd7d83df0dc6
Leos Stejskal <lstejska@redhat.com>
2020-03-23 09:00:09 +0100

    Leapp PreUpgrade Action

--- app/models/preupgrade_report.rb
+++ app/models/preupgrade_report.rb
@@ -3,21 +3,16 @@
 class PreupgradeReport < ::Report
   has_many :preupgrade_report_entries, dependent: :destroy
 
-  def self.create_report(host, date, status, data)
-    report = PreupgradeReport.create(:host => host,
-                                     :reported_at => date,
-                                     :status => status)
-    data[:entries]&.each do |entry|
-      data = entry.slice(:title, :actor, :summary, :audience, :severity)
-                  .merge(:preupgrade_report => report,
-                         :hostname => host.hostname,
-                         :tags => entry[:tags].to_json,
-                         :detail => entry[:detail].to_json,
-                         :leapp_run_id => data[:leapp_run_id])
-      data.permit!
-      PreupgradeReportEntry.create! data
+  def self.create_report(host, data)
+    report = PreupgradeReport.create(host: host, status: 0, reported_at: DateTime.now.utc)
+
+    data['entries']&.map! do |entry|
+      entry.except('timeStamp', 'id')
+           .merge(preupgrade_report: report,
+                  hostname: host.hostname,
+                  leapp_run_id: data['leapp_run_id'])
     end
-    report.save!
-    report
+
+    PreupgradeReportEntry.create!(data['entries'])
   end
 end
--- app/models/preupgrade_report_entry.rb
+++ app/models/preupgrade_report_entry.rb
@@ -3,5 +3,11 @@
 class PreupgradeReportEntry < ApplicationRecord
   belongs_to :preupgrade_report
 
-  validates :preupgrade_report_id, :presence => true
+  validates :preupgrade_report_id, presence: true
+  validates :hostname, presence: true
+  validates :title, presence: true
+  validates :actor, presence: true
+  validates :audience, presence: true
+  validates :severity, presence: true
+  validates :leapp_run_id, presence: true
 end
