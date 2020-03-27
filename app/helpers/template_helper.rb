94e22476133c83172910c6cfcf0918437c1501bc
Leos Stejskal <lstejska@redhat.com>
2020-03-27 12:46:39 +0100

    Leapp Remediation plan - update after review

--- app/helpers/template_helper.rb
+++ app/helpers/template_helper.rb
@@ -1,10 +1,13 @@
   module TemplateHelper
     def build_remediation_plan(remediation_ids, host)
-      entries = PreupgradeReportEntry.where(id: remediation_ids.split(','), host_id: host.id)
+      entries = PreupgradeReportEntry.where(id: remediation_ids, host: host)
+                    .to_a.map { |e| JSON.parse(e.detail) }
+                    .compact
       result = ""
 
-      entries&.each do |entry|
-        JSON.parse(entry.detail.gsub('=>',':'))['remediations']&.each do |remediation|
+      entries.each do |entry|
+        entry['remediations']&.each do |remediation|
+          next unless remediation['type'] == 'command'
           result << "#{remediation['context'].join(' ')}\n"
         end
       end
