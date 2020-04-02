8ebedaaa23fb5674b042394ff028e64d262eca87
Leos Stejskal <lstejska@redhat.com>
2020-04-02 08:19:32 +0200

    Rerun button - update after review

--- app/helpers/concerns/foreman_leapp/remote_execution_helper_extension.rb
+++ app/helpers/concerns/foreman_leapp/remote_execution_helper_extension.rb
@@ -5,11 +5,11 @@ module ForemanLeapp
     def job_invocation_task_buttons(task)
       return super unless @job_invocation.remote_execution_feature&.label == 'leapp_remediation_plan'
 
-      super << link_to(_('Rerun preupgrade check'),
-                       new_job_invocation_path(host_ids: @resource_base.map(&:id), feature: 'leapp_preupgrade'),
-                       class: 'btn btn-default',
-                       title: _('Run Leapp Preupgrade check again'),
-                       method: :get)
+      super.insert(2, link_to(_('Rerun preupgrade check'),
+                      new_job_invocation_path(host_ids: @resource_base.map(&:id), feature: 'leapp_preupgrade'),
+                      class: 'btn btn-default',
+                      title: _('Run Leapp Preupgrade check again'),
+                      method: :get))
     end
   end
 end
