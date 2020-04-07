a68f57e84700dafa8de4e2fb396085b8bc453202
Leos Stejskal <lstejska@redhat.com>
2020-04-07 11:20:34 +0200

    Leapp - fix feature condition, update after review

--- app/lib/helpers/job_helper.rb
+++ app/lib/helpers/job_helper.rb
@@ -6,9 +6,8 @@ module Helpers
       def correct_feature?(job_invocation, feature)
         job_invocation.job_category == ::ForemanLeapp::JOB_CATEGORY &&
           RemoteExecutionFeature.find_by(job_template_id: job_invocation.pattern_template_invocations
-                                                                        .first
-                                                                        &.template_id)
-            &.label == feature
+                                                                        .pluck(:template_id)
+                                                                        .first)&.label == feature
       end
     end
   end
