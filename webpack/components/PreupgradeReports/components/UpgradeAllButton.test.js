b1d2a2e753877b4631ad7c4fac361c4aefcf6dc9
Leos Stejskal <lstejska@redhat.com>
2020-04-21 15:33:25 +0200

    Leapp Preupgrade report - Add upgrade button

---
+++ webpack/components/PreupgradeReports/components/UpgradeAllButton.test.js
@@ -0,0 +1,18 @@
+import { testComponentSnapshotsWithFixtures } from '@theforeman/test';
+
+import UpgradeAllButton from './UpgradeAllButton';
+
+import { preupgradeReports } from '../__tests__/PreupgradeReports.fixtures';
+
+const fixtures = {
+  'should render': {
+    preupgradeReports,
+    postUrl: '/job_invocations/new',
+    disabled: false,
+    title: 'Button title',
+    csrfToken: 'abcd',
+  },
+};
+
+describe('UpgradeAllButton', () =>
+  testComponentSnapshotsWithFixtures(UpgradeAllButton, fixtures));
