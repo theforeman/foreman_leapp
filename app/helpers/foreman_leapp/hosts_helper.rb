213355e8e0642e2ee7d4327f839d3de28b589b42
Leos Stejskal <lstejska@redhat.com>
2022-04-28 10:00:16 +0200

    Rubocop

--- app/helpers/foreman_leapp/hosts_helper.rb
+++ app/helpers/foreman_leapp/hosts_helper.rb
@@ -3,9 +3,9 @@
 module ForemanLeapp
   module HostsHelper
     def leapp_hosts_multiple_actions
-      [{ action: [_('Preupgrade check with Leapp'), new_job_invocation_path(feature: 'leapp_preupgrade'), false], priority: 1000},
-       {action: [_('Upgrade with Leapp'), new_job_invocation_path(feature: 'leapp_upgrade'), false], priority: 1000}
-      ]
+      [{ action: [_('Preupgrade check with Leapp'), new_job_invocation_path(feature: 'leapp_preupgrade'), false],
+         priority: 1000 },
+       { action: [_('Upgrade with Leapp'), new_job_invocation_path(feature: 'leapp_upgrade'), false], priority: 1000 }]
     end
   end
 end
