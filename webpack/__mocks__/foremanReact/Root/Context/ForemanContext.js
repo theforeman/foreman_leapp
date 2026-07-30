d3a8b186a27e09b5b05e1e8b4ba1532aacd26a4e
Andrei Lakatos <andreilakatos.dev@gmail.com>
2026-07-30 08:15:46 -0400

    Fixes #39567 - add test_setup file that imports foremanJSTestSetup and fix lint

--- webpack/__mocks__/foremanReact/Root/Context/ForemanContext.js
+++ webpack/__mocks__/foremanReact/Root/Context/ForemanContext.js
@@ -1,4 +1,7 @@
+const DEFAULT_PER_PAGE = 20;
+const PER_PAGE_30 = 30;
+
 export const useForemanSettings = () => ({
-  perPage: 20,
-  perPageOptions: [5, 10, 20, 30, 50],
+  perPage: DEFAULT_PER_PAGE,
+  perPageOptions: [5, 10, DEFAULT_PER_PAGE, PER_PAGE_30, 50],
 });
--- webpack/global_index.js
+++ webpack/global_index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { addGlobalFill } from 'foremanReact/components/common/Fill/GlobalFill';
 import PreupgradeReportsTable from './components/PreupgradeReportsTable';
 
+const LEAPP_PREUPGRADE_REPORT_FILL_WEIGHT = 100;
+
 addGlobalFill(
   'job-invocation-additional-info',
   'leapp-preupgrade-report-fill',
   <PreupgradeReportsTable key="leapp-report-fill" />,
-  100
+  LEAPP_PREUPGRADE_REPORT_FILL_WEIGHT
 );
